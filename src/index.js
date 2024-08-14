357c169c7207ce757977fc5a5fbaad1dc2ce7841
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2024-08-14 10:53:47 -0300

    index + singup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Form from './components/form';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/cadastro",
+    element: <Form />,
+  },
+
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <RouterProvider router={router} />
+  </>
 );
