200e59658b644ed05fd04e9cf90cd077ebb66c1d
tomas-nogueira <tomasmnogueira2006@gmail.com>
2024-08-14 10:59:41 -0300

    tomas alt

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+      <h1>TESTE</h1>
+    </div>
+  )
+}
+
+export default navbar
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/navbar'
+
+function home() {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default home
